9fc188d65d60963881882d9d677feb24d63504bd
hiimkoin <nikoebener@gmail.com>
2018-12-03 18:17:02 +0100

    added p2

--- 3.py
+++ 3.py
@@ -2,20 +2,27 @@ from re import search
 from collections import defaultdict
 
 claim = [f.strip() for f in open('3.txt', 'r')]
-
-# p1
 fields = defaultdict(int)
-for c in claim:
-    m = search(r'#(\d*) @ (\d*),(\d*): (\d*)x(\d*)', c)
-    parsed = list(map(int, m.groups()))
-    
-    id, leftpad, toppad, width, height \
-        = parsed[0], parsed[1], parsed[2], parsed[3], parsed[4]
-    
-    for y in range(toppad, toppad + height):
-        for x in range(leftpad, leftpad + width):
-            fields[(y, x)] += 1
 
-print(sum(1 for i in fields.values() if i > 1))
+for part in range(1, 3):
+    for c in claim:
 
-# p2
+        m = search(r'#(\d*) @ (\d*),(\d*): (\d*)x(\d*)', c)
+        parsed = list(map(int, m.groups()))
+        
+        id, leftpad, toppad, width, height \
+            = parsed[0], parsed[1], parsed[2], parsed[3], parsed[4]
+        
+        overlaps = False
+
+        for y in range(toppad, toppad + height):
+            for x in range(leftpad, leftpad + width):
+                if part == 2 and fields[(y, x)] > 1:
+                    overlaps = True
+                fields[(y, x)] += 1
+
+        if part == 2 and overlaps == False: # p2
+            print(id)
+
+    if part == 1: # p1
+        print(sum(1 for i in fields.values() if i > 1))

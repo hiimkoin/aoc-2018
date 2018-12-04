342e3aee21b3058d16d90f5bcf05a3b280903873
hiimkoin <nikoebener@gmail.com>
2018-12-04 23:10:23 +0100

    day 4 p2

--- 4.py
+++ 4.py
@@ -4,7 +4,6 @@ from datetime import datetime
 logs = [f.strip() for f in open('input/4.txt', 'r')]
 logs.sort(key=lambda l:datetime.strptime(l[1:17], '%Y-%m-%d %H:%M'))
 
-# p1
 guards = {}
 current_guard = None
 
@@ -23,8 +22,13 @@ for i, log in enumerate(logs):
         for j in range(int(ts[14:]), int(logs[i + 1][15:17])):
             guards[current_guard]['time'][j] += 1
 
-most = [(key, sum(guards[key]['time'])) for key, _ in guards.items()]
-guard = max(most, key=lambda item:item[1])
-minute = guards[guard[0]]['time'].index(max(guards[guard[0]]['time']))
+for part in range(1, 3):
+    most = None
+    if part == 1: # p1
+        most = [(key, sum(guards[key]['time'])) for key, _ in guards.items()]
+    else: # p2
+        most = [(key, max(guards[key]['time'])) for key, _ in guards.items()]
+    guard = max(most, key=lambda item:item[1])
+    minute = guards[guard[0]]['time'].index(max(guards[guard[0]]['time']))
 
-print(int(guard[0]) * int(minute))
+    print(int(guard[0]) * int(minute))

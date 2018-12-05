e70cde4cb28fe7493591571e227c8e39f33cf683
hiimkoin <nikoebener@gmail.com>
2018-12-05 15:47:32 +0100

    day 5 p2

--- 5.py
+++ 5.py
@@ -1,13 +1,26 @@
+from string import ascii_lowercase
+
 input = [f for f in open('input/5', 'r')][0]
 
-# p1
-stack = []
-for char in input:
-    if len(stack) == 0:
-        stack.append(char)
-    elif stack[-1].lower() == char.lower() and stack[-1] != char:
-        stack.pop()
-    else:
-        stack.append(char)
+small = []
+for char in '0' + ascii_lowercase:
+    
+    if char != '0': # p2
+        input2 = input.replace(char, '')
+        input2 = input2.replace(char.upper(), '')
+    else: # p1
+        input2 = input
 
-print(len(stack))
+    stack = []
+    for char in input2:
+        if len(stack) == 0:
+            stack.append(char)
+        elif stack[-1].lower() == char.lower() and stack[-1] != char:
+            stack.pop()
+        else:
+            stack.append(char)
+
+    small.append(len(stack))
+
+print(small[0]) # p1
+print(min(small)) # p2
